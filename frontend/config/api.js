872db02fe1d111df6145826dbdb17480a85c986c
ShineyVamp <shineyvampire@gmail.com>
2025-12-14 18:21:52 +0700

    Add files via upload

---
+++ frontend/config/api.js
@@ -0,0 +1,7 @@
+/**
+ * Konfigurasi API Terpusat.
+ * Mengatur URL backend di satu tempat agar mudah dikelola.
+ * Jika deploy, cukup ubah URL di sini.
+ */
+
+export const API_URL = 'http://localhost:5000/api';
