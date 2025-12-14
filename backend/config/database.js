708c086a42362425583dffb5e9d77c88a47ff58b
ShineyVamp <shineyvampire@gmail.com>
2025-12-14 20:10:34 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,10 +6,11 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 4000, // TiDB biasanya port 4000
+    port: process.env.DB_PORT || 4000,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 5,
     queueLimit: 0,
+    connectTimeout: 60000,
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: false
@@ -26,4 +27,5 @@ db.getConnection((err, connection) => {
 });
 
 module.exports = db;
+
 
